c0ecdc82dde937bf564fa73737ba1639c2fe236e
Daniel Ormsby <ormsbydaniel@gmaill.com>
2025-12-19 10:29:56 +0000

    Adding dev-plotly-chart to preview

--- public/viewer/update-viewer-bundle.sh
+++ public/viewer/update-viewer-bundle.sh
@@ -3,7 +3,7 @@
 # Configuration
 VIEWER_DIR=~/software/apex_geospatial_explorer
 BUILDER_DIR=~/software/apex-ge-config-builder
-VERSION="3.4.2"
+VERSION="dev-plotly-chart"
 
 # Colors for output
 GREEN='\033[0;32m'
