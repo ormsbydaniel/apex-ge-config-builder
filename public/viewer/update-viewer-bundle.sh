cf2b11a8d32e4c077c968bc9b4fa86cca6a71309
Daniel Ormsby <ormsbydaniel@gmaill.com>
2026-01-29 10:55:34 +0000

    Adding 3.5.0

--- public/viewer/update-viewer-bundle.sh
+++ public/viewer/update-viewer-bundle.sh
@@ -3,7 +3,7 @@
 # Configuration
 VIEWER_DIR=~/software/apex_geospatial_explorer
 BUILDER_DIR=~/software/apex-ge-config-builder
-VERSION="dev-3.5-candidate"
+VERSION="3.5.0"
 
 # Colors for output
 GREEN='\033[0;32m'
